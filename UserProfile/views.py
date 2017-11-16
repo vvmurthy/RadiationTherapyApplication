45bfd58388cad754f60f46fdc908407f75ae1fe8
ipilab <ipilab@egg120-9.usc.edu>
2017-11-16 03:40:23 -0800

    Added signup functionality

--- UserProfile/views.py
+++ UserProfile/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -12,4 +15,17 @@ def login(request):
 	return render(request, 'users/login/login.html')
 
 def signup(request):
-	return render(request, 'users/signup/signup.html')
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			raw_password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=raw_password)
+			auth_login(request, user)
+			return redirect('home')
+	else:
+		form = UserCreationForm()
+
+	return render(request, 'users/signup/signup.html', {'form': form})
