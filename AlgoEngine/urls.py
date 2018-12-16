ad2fd54cccc8ab9f21a84cae0769b2ad8df1d471
Veda Murthy <vvmurthy@usc.edu>
2018-12-15 21:10:31 -0800

    added base DVH

--- AlgoEngine/urls.py
+++ AlgoEngine/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
 	url(r'^ct_images/(?P<patient_id>[-\w]+)/(?P<study_id>[-\w]+)/(?P<series_id>[-\w]+)/(?P<roi_index>[-\w]+)/(?P<index>[-\w]+)/(?P<checked>\w+)/$', views.update_ct, name='update'),
 	url(r'^ct_images/(?P<patient_id>[-\w]+)/(?P<study_id>[-\w]+)/(?P<series_id>[-\w]+)/$', views.get_ct, name='view_cts'),
 
+	# Get dvh
+	url(r'^dvh/(?P<patient_id>[-\w]+)/(?P<study_id>[-\w]+)/(?P<series_id>[-\w]+)/(?P<roi_index>[-\w]+)/$', views.get_dvh, name="dvh"),
+
 	# Get the plan data
 	url(r'^plan/(?P<patient_id>[-\w]+)/(?P<study_id>[-\w]+)/(?P<series_id>[-\w]+)/$', views.get_ct, name='view_cts'),
 
--- AlgoEngine/views.py
+++ AlgoEngine/views.py
@@ -3,6 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+
+from AlgoEngine.app.AlgoEngine.RadiationTherapyDecisionSupport.Python.AlgoEngine import AlgoManager
+from AlgoEngine.app.AlgoEngine.RadiationTherapyDecisionSupport.Python.AlgoEngine.utils import *
+
 from dicomdb.utils import getImageBlock
 
 import os
@@ -23,32 +27,48 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, JsonResponse
 
 
-
 # Create your views here.
-def make_ct_image(current_ct, ct_block):
+def make_ct_image(current_ct, ct_block, mask=None):
 	fig = plt.figure()
 	ax = fig.add_subplot(1,1,1)
 	fig.subplots_adjust(wspace=0, hspace=0)
-	ax.imshow(ct_block[:, :, current_ct], cmap='gray')
+	im = np.copy(ct_block[:, :, current_ct])
+	rgb = np.zeros((im.shape[0], im.shape[1], 3), dtype=np.uint8)
+	rgb[:, :, 0] = im
+	rgb[:, :, 1] = im
+	rgb[:, :, 2] = im
+	if mask is not None:
+		rgb[mask == 1, :] = [255, 0, 0]
+
+	ax.imshow(rgb)
 	ax.axis('off')
 	plt.tight_layout()
 	img = BytesIO()
 	fig.savefig(img, format="png", bbox_inches='tight', pad_inches=0, transparent=True)
+	plt.close()
 	return img
 
-
+@login_required(login_url="/users/login/")
 def update_ct(request, patient_id, study_id, series_id, roi_index, index, checked):
 	patient = Patient.objects.get(id=patient_id)
 	rootDir = os.path.join(PatientDirBase, patient.PatientName)
-	cts, _ = getImageBlock(rootDir, patient_id, study_id, series_id)
+	cts, sopIds = getImageBlock(rootDir, patient_id, study_id, series_id)
 	checkBox = False
 	if "true" in checked:
 		checkBox = True
 
 	# Overlay the contour if selected
+	roi = None
+	if checkBox:
+		am = AlgoManager(study_id, False)
+		contours, imagePatientPosition = am.get_contours_by_id(int(roi_index))
+		contour_block, roi_block = contours[int(roi_index)]
+		roi_block = convertROIToCTSpace(contour_block, imagePatientPosition, sopIds)
+		roi = roi_block[:, :, int(index)]
 
 	current_ct = int(index)
-	image = make_ct_image(current_ct, cts)
+	image = make_ct_image(current_ct, cts, roi)
+
 	return HttpResponse(image.getvalue(), content_type="image/png")
 
 @login_required(login_url='/users/login/')
@@ -62,4 +82,22 @@ def get_ct(request, patient_id, study_id, series_id):
 		return HttpResponse(image.getvalue(), content_type="image/png")
 	else:
 		return HttpResponse(status=401)
+
+@login_required(login_url="/users/login/")
+def get_dvh(request, patient_id, study_id, series_id, roi_index):
+	rt_roi = RTROI.objects.get(id=roi_index)
+	dvh = RTDVH.objects.get(DVHReferencedROI=rt_roi)
+
+	fig = plt.figure()
+	ax = fig.add_subplot(1,1,1)
+	fig.subplots_adjust(wspace=0, hspace=0)
+	rgb = np.zeros((100, 100, 3), dtype=np.uint8)
+
+	ax.imshow(rgb)
+	ax.axis('off')
+	plt.tight_layout()
+	img = BytesIO()
+	fig.savefig(img, format="png", bbox_inches='tight', pad_inches=0, transparent=True)
+	plt.close()
+	return HttpResponse(img.getvalue(), content_type="image/png")
 
